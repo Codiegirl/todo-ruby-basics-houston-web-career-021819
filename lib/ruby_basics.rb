259286d0e6c9d1f79c614da0b54c40031a978b0e
Ro Flo <iroflood@msn.com>
2019-02-13 23:57:18 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,42 @@
 def division(num1, num2)
- 
+  num1 / num2
 end
+division(6, 1)
+
+
+
+
 
 def assign_variable(value)
-  
-end
+  value = "Bob"
+  end
+  assign_variable("Bob")
 
-def argue
-  
-end
+def argue (phrase)
+  phrase = "I'm right and you are wrong!"
+  end
+  argue (" I'm right and you are wrong!")
 
-def greeting
-
-end
+def greeting (g, n)
+  greeting = g
+  name = n
+  return g + " " + n
+    end
+# greeting ("Hi there," "Bobby")
 
 def return_a_value
-
-end
+  phrase = "Nice"
+  end
 
 def last_evaluated_value
+  phrase = "expert"
 
 end
 
 def pizza_party
+  retrun "cheese"
+end
 
+def pizza_party("pepperoni")
+  retrun "pepperoni"
 end
